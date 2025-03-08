fbc24ac04b010a901283b4c6d9287d3c989bd64e
alex-sarson <alex.luke.sarson@gmail.com>
2025-03-08 20:34:05 +0000

    working life total altering buttons

--- app/components/Player.tsx
+++ app/components/Player.tsx
@@ -1,19 +1,60 @@
-import { Text, View, type ViewProps } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, type ViewProps } from "react-native";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
-export type PlayerProps = ViewProps & {
-  player?: number;
-};
-
-export default function Player({ player }: PlayerProps) {
+export default function Player() {
+  const [life, setLife] = useState(40);
+  const gainLife = () => {
+    setLife(life + 1);
+  };
+  const loseLife = () => {
+    setLife(life - 1);
+  };
   return (
-    <View
-      style={{
-        backgroundColor: "green",
-        height: "100%",
-        width: "100%",
-      }}
-    >
-      <Text>This is player {player}</Text>
+    <View style={[styles.container]}>
+      <FontAwesome.Button
+        name="minus"
+        size={30}
+        color={"white"}
+        backgroundColor={"transparent"}
+        onPress={loseLife}
+      />
+      <Text style={[styles.life]}>{life}</Text>
+      <FontAwesome.Button
+        name="plus"
+        size={30}
+        color={"white"}
+        backgroundColor={"transparent"}
+        onPress={gainLife}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    borderWidth: 2,
+    borderStyle: "solid",
+    borderColor: "green",
+    backgroundColor: "pink",
+    height: "100%",
+    width: "50%",
+    maxWidth: "70%",
+    marginRight: "auto",
+    marginLeft: "auto",
+    marginTop: 0,
+    marginBottom: 0,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  life: {
+    fontSize: 120,
+    color: "white",
+    marginTop: 0,
+    marginBottom: 0,
+    marginLeft: 40,
+    marginRight: 40,
+  },
+});
--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,49 @@
 import { Link } from "expo-router";
-import { Pressable, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import Player from "./components/Player";
 
 export default function HomeScreen() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        width: "100%",
-        height: "100%",
-      }}
-    >
-      <Text>This is the homepage</Text>
-      <Link href="/settings" asChild>
+    <View style={[styles.container]}>
+      <Link href="/settings" asChild style={[styles.settings]}>
         <Pressable>
           <Text>Settings</Text>
         </Pressable>
       </Link>
+      <View style={[styles.playerWrapper, styles.invert]}>
+        {/* player's 1/1&2 */}
+        <Player />
+        <Player />
+      </View>
+      <View style={[styles.playerWrapper]}>
+        {/* player's 2/3&4 */}
+        <Player />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    height: "100%",
+  },
+  settings: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+  },
+  playerWrapper: {
+    display: "flex",
+    height: "50%",
+    width: "100%",
+    flexDirection: "row",
+  },
+  invert: {
+    transform: "rotate(180deg)",
+  },
+});
